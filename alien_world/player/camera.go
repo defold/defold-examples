d5462830697fc4b3ad0180d586085908d93a336a
Mikael Säker <mikael@sicher.org>
2016-11-16 15:27:25 +0000

    Alien world project

---
+++ alien_world/player/camera.go
@@ -0,0 +1,30 @@
+components {
+  id: "camera"
+  component: "/player/camera.camera"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/player/camera.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
