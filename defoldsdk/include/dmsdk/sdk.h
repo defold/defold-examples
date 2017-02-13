99b049e5646d987d36e5bde59e4e1b75267212d0
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-02-13 15:58:25 +0100

    Added defoldsdk headers

---
+++ defoldsdk/include/dmsdk/sdk.h
@@ -0,0 +1,18 @@
+#ifndef DMSDK_SDK_H
+#define DMSDK_SDK_H
+
+#include <dmsdk/graphics/graphics_native.h>
+#include <dmsdk/script/script.h>
+#include <dmsdk/extension/extension.h>
+#include <dmsdk/lua/luaconf.h>
+#include <dmsdk/lua/lua.h>
+#include <dmsdk/lua/lauxlib.h>
+#include <dmsdk/dlib/buffer.h>
+#include <dmsdk/dlib/log.h>
+#include <dmsdk/dlib/array.h>
+#include <dmsdk/dlib/hash.h>
+#include <dmsdk/dlib/configfile.h>
+#include <dmsdk/dlib/shared_library.h>
+#include <dmsdk/dlib/align.h>
+
+#endif // DMSDK_SDK_H
